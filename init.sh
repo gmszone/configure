312cca7db263c66b6866b18dccbdcf58af738c91
gmszone <gmszone@qq.com>
2014-05-22 09:57:57 +0800

    update the install configure scripts

--- init.sh
+++ init.sh
@@ -1,14 +1,17 @@
 
-echo "clone bash-it" 
+echo -e "\033[Install bash-it\033[0m" 
 git clone https://github.com/revans/bash-it .bash_it
-# sh ~/.bash_it/install.sh
+sh ~/.bash_it/install.sh
 
-echo "install janus"
-# curl -Lo- https://bit.ly/janus-bootstrap | bash
+echo -e "\033[Install janus\033[0m"
+curl -Lo- https://bit.ly/janus-bootstrap | bash
 
-echo "clone purcell emacs configure"
+echo -e "\033[Install purcell emacs configure\033[0m"
 git clone https://github.com/purcell/emacs.d .emacs.d
 emacs
 
-echo "clone zsh configure"
+echo -e "\033[Install zsh configure\033[0m"
 git clone https://github.com/robbyrussell/oh-my-zsh.git
+
+echo -e "\033[Install RVM\033[0m"
+\curl -sSL https://get.rvm.io | bash -s stable
