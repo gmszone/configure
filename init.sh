674cfdbdf6c1d7c4fc6234214f98cf70bc279187
phodal <phodal@linux.site>
2014-06-02 11:54:32 +0800

    add the composer & chruby remove

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ case $1 in
     emacs
 
     echo -e "\033[Install zsh configure\033[0m"
-    git clone https://github.com/robbyrussell/oh-my-zsh.git
+    git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 
     ;;
   chruby)
@@ -30,8 +30,15 @@ case $1 in
     tar -xzvf chruby-0.3.8.tar.gz
     cd chruby-0.3.8/
     sudo make install
+    rm -rf chruby-0.3.8
+
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
     ;;
   drush)
+    curl -sS https://getcomposer.org/installer | php
     sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
     source $HOME/.bashrc
     composer global require drush/drush:dev-master
@@ -40,7 +47,9 @@ case $1 in
     sudo pip install argcomplete
     sudo pip install mezzanine
     sudo pip install pyserial
+    sudo pip install virtualenvwrapper
     ;;
+
   ubuntu)
     sudo apt-get install curl
     ;;
